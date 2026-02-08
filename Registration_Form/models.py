e887b45a8470914cce79aabd4a5096fc3f728fc5
Datta <dasdattatraya@gmail.com>
2026-02-08 21:18:37 +0530

    code

---
+++ Registration_Form/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Registration(models.Model):
+    name = models.CharField(max_length=64)
+    employee_id = models.CharField(max_length=64,unique=True)
+    age = models.PositiveIntegerField()
+    coupon = models.CharField(max_length=64)
+    email = models.EmailField()
+    class Meta:
+        db_table='Table_Registration'
+
+
+    
+    def __str__(self):
+        return self.name
+
